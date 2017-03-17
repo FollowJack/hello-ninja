b0c857c1a977b0a071553bdf1c5072cdda7a9eee
Deniel Horvatic <deniel.horvatic@porscheidigitallab.com>
2017-03-17 17:22:35 +0100

    Added missing files

---
+++ src/main/java/de/mhp/CloudNinjasApplication.java
@@ -0,0 +1,12 @@
+package de.mhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNinjasApplication.class, args);
+	}
+}
