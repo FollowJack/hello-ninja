f9b074009fdc03ad81f6350dd172c88afdb37c53
Deniel Horvatic <deniel.horvatic@porscheidigitallab.com>
2017-03-17 11:34:38 +0100

    Initial commit

---
+++ src/main/java/de/mhp/HelloNinjaApplication.java
@@ -0,0 +1,12 @@
+package de.mhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloNinjaApplication.class, args);
+	}
+}
